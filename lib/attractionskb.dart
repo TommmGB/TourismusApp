c9e73c35a49cc791a0df10b43bbe69d788316648
TommmGB <112927088+TommmGB@users.noreply.github.com>
2022-09-07 12:43:15 +0200

    Update attractionskb.dart

--- lib/attractionskb.dart
+++ lib/attractionskb.dart
@@ -9,7 +9,7 @@ class AttractionsKB extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Page'),
+        title: Text('Welcome to Kleinbasel!'),
       ),
     );
   }
