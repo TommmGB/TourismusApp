f45150286c49dbf01d2206e1bc4bc0232526f736
TommmGB <112927088+TommmGB@users.noreply.github.com>
2022-09-08 16:28:25 +0200

    Create tinguelymuseum.dart

---
+++ lib/tinguelymuseum.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TinguelyMuseum extends StatelessWidget {
+  const TinguelyMuseum({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tinguely Museum'),
+      ),
+    );
+  }
+}
