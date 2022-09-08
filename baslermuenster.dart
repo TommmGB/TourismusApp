06dc7f71a4d83f624d01e4eeeb41b78311ad051f
TommmGB <112927088+TommmGB@users.noreply.github.com>
2022-09-08 18:46:45 +0200

    Add files via upload

---
+++ baslermuenster.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BaslerMuenster extends StatelessWidget {
+  const BaslerMuenster({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Basler Münster'),
+      ),
+    );
+  }
+}
