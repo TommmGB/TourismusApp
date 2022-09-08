88471d5172bb817be8b9fb67bf598deed26a703c
TommmGB <112927088+TommmGB@users.noreply.github.com>
2022-09-08 16:22:54 +0200

    Update attractionskb.dart

--- lib/attractionskb.dart
+++ lib/attractionskb.dart
@@ -14,7 +14,7 @@ class AttractionsKB extends StatelessWidget {
               Container(
                 margin:
                     const EdgeInsets.symmetric(vertical: 10, horizontal: 70),
-                child: const Text('Kleinbasel',
+                child: const Text('Grossbasel',
                     style: TextStyle(
                       fontFamily: "Nunito",
                       fontWeight: FontWeight.bold,
@@ -49,7 +49,7 @@ class AttractionsKB extends StatelessWidget {
               SizedBox(height: 10),
               GestureDetector(
                   onTap: () {
-                    Navigator.pushNamed(context, '/a');
+                    Navigator.pushNamed(context, '/b');
                   },
                   child: Card(
                       elevation: 10,
@@ -57,7 +57,7 @@ class AttractionsKB extends StatelessWidget {
                         tileColor: Colors.lightBlueAccent[100],
                         leading: Icon(Icons.location_on_outlined),
                         title: Text(
-                          'Kleinbasel',
+                          'Grossbasel',
                           style: TextStyle(
                               fontFamily: "Nunito",
                               fontSize: 25,
@@ -106,7 +106,7 @@ class AttractionsKB extends StatelessWidget {
               pinned: true,
               floating: false,
               expandedHeight: 150,
-              backgroundColor: Colors.orangeAccent[200],
+              backgroundColor: Colors.lightGreenAccent[100],
               flexibleSpace: FlexibleSpaceBar(
                 title: const Text("Discover Kleinbasel!",
                     style: TextStyle(
@@ -116,36 +116,51 @@ class AttractionsKB extends StatelessWidget {
                     )),
               ),
             ),
-            SliverList(
-              delegate: SliverChildListDelegate(
-                [
-                  Stack(
-                    children: <Widget>[
-                      GestureDetector(
-                          onTap: () {
-                            Navigator.pushNamed(context, '/a');
-                          },
-                          child: SizedBox(
-                              height: 230,
-                              width: 400,
-                              child: Image.network(
-                                  'https://cdn.tomas-travel.com/tds/repository/TDS00020012496817936/TDS00020010010367324/TDS00020013696921880_sized_800_0.jpg',
-                                  fit: BoxFit.cover))),
-                      Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 0, horizontal: 0),
-                        child: const Card(
-                            color: Colors.lightBlueAccent,
-                            child: Text("Basler Münster",
-                                style: TextStyle(
-                                    fontFamily: "Nunito",
-                                    fontSize: 25,
-                                    color: Colors.purple))),
-                      ),
-                    ],
-                  ),
-                ],
+            SliverGrid(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1,
+                mainAxisSpacing: 0,
+                crossAxisSpacing: 10,
+                childAspectRatio: 2.0,
               ),
+              delegate: SliverChildListDelegate([
+                Stack(children: <Widget>[
+                  GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/c');
+                      },
+                      child: SizedBox(
+                          height: 300,
+                          width: 400,
+                          child: Card(
+                              borderOnForeground: true,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.only(
+                                      bottomRight: Radius.circular(5),
+                                      topRight: Radius.circular(5)),
+                                  side: BorderSide(
+                                      width: 5, color: Colors.black)),
+                              child: Image.network(
+                                  'https://www.baselferien.ch/bcwa/wp-content/gf-uploads/2018/02/basler-muenster-basel-09.jpg',
+                                  fit: BoxFit.cover)))),
+                  Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 5),
+                      child: const Card(
+                          borderOnForeground: true,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(5),
+                                  topRight: Radius.circular(5)),
+                              side: BorderSide(width: 1, color: Colors.black)),
+                          color: Colors.lightBlueAccent,
+                          child: Text("Basler Münster",
+                              style: TextStyle(
+                                  fontFamily: "Nunito",
+                                  fontSize: 25,
+                                  color: Colors.purple))))
+                ]),
+              ]),
             ),
           ],
         ),
