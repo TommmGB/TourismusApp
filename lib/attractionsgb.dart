b6d2223ff0f1b3bc080ebd68cea53878fb00c75a
TommmGB <112927088+TommmGB@users.noreply.github.com>
2022-09-08 14:31:43 +0200

    Add files via upload

---
+++ lib/attractionsgb.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AttractionsGB extends StatelessWidget {
+  const AttractionsGB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Page'),
+      ),
+    );
+  }
+}
