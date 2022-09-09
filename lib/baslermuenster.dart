99f933866c2758abe4da332f9809bcfce81db06b
TommmGB <112927088+TommmGB@users.noreply.github.com>
2022-09-09 15:00:50 +0200

    Update baslermuenster.dart

--- lib/baslermuenster.dart
+++ lib/baslermuenster.dart
@@ -1,14 +1,380 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+final Uri url = Uri.parse("https://www.tinguely.ch/en.html");
 
 class BaslerMuenster extends StatelessWidget {
   const BaslerMuenster({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Basler Muenster'),
-      ),
-    );
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+                iconTheme: IconThemeData(color: Colors.black),
+                backgroundColor: Colors.orangeAccent[100],
+                title: Text('Tinguely Museum',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontFamily: 'Nunito',
+                      fontSize: 30,
+                    ))),
+            bottomNavigationBar: BottomAppBar(
+                color: Colors.orangeAccent[100],
+                child: new Row(children: [
+                  Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 2),
+                      child: IconButton(
+                          icon: Icon(Icons.arrow_back_outlined),
+                          iconSize: 25,
+                          onPressed: (() {
+                            Navigator.pushNamed(context, 'Homepage');
+                          }))),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: Text(
+                      'Previous',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Nunito',
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 55),
+                    child: Text('Next',
+                        style: TextStyle(
+                          fontFamily: 'Nunito',
+                          fontSize: 20,
+                        )),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0),
+                      child: IconButton(
+                          icon: Icon(Icons.arrow_forward_outlined),
+                          iconSize: 25,
+                          onPressed: () {
+                            Navigator.pushNamed(context, 'Homepage');
+                          })),
+                ])),
+            drawer: Drawer(
+                backgroundColor: Colors.orangeAccent[100],
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 70),
+                      child: const Text('Tinguely Museum',
+                          style: TextStyle(
+                            fontFamily: "Nunito",
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                            color: Colors.blue,
+                          )),
+                    ),
+                    const Divider(
+                      thickness: 2,
+                      indent: 0,
+                      endIndent: 0,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, 'Homepage');
+                        },
+                        child: Card(
+                            elevation: 10,
+                            child: ListTile(
+                              tileColor: Colors.lightBlueAccent[100],
+                              leading: Icon(Icons.home_outlined),
+                              title: Text(
+                                'Homepage',
+                                style: TextStyle(
+                                    fontFamily: "Nunito",
+                                    fontSize: 25,
+                                    color: Colors.purple),
+                              ),
+                            ))),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/a');
+                        },
+                        child: Card(
+                            elevation: 10,
+                            child: ListTile(
+                              tileColor: Colors.lightBlueAccent[100],
+                              leading: Icon(Icons.location_on_outlined),
+                              title: Text(
+                                'Kleinbasel',
+                                style: TextStyle(
+                                    fontFamily: "Nunito",
+                                    fontSize: 25,
+                                    color: Colors.purple),
+                              ),
+                            ))),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/b');
+                        },
+                        child: Card(
+                            elevation: 10,
+                            child: ListTile(
+                              tileColor: Colors.lightBlueAccent[100],
+                              leading: Icon(Icons.location_on_outlined),
+                              title: Text(
+                                'Grossbasel',
+                                style: TextStyle(
+                                    fontFamily: "Nunito",
+                                    fontSize: 25,
+                                    color: Colors.purple),
+                              ),
+                            ))),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                        child: Card(
+                            elevation: 10,
+                            child: ListTile(
+                              leading: Icon(Icons.tram_outlined),
+                              tileColor: Colors.lightBlueAccent[100],
+                              title: Text(
+                                'Public Transport',
+                                style: TextStyle(
+                                    fontFamily: "Nunito",
+                                    fontSize: 25,
+                                    color: Colors.purple),
+                              ),
+                            ))),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                        child: Card(
+                            elevation: 10,
+                            child: ListTile(
+                              leading: Icon(Icons.map_outlined),
+                              tileColor: Colors.lightBlueAccent[100],
+                              title: Text(
+                                'Interactive Map',
+                                style: TextStyle(
+                                    fontFamily: "Nunito",
+                                    fontSize: 25,
+                                    color: Colors.purple),
+                              ),
+                            ))),
+                    SizedBox(height: 10),
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/a');
+                        },
+                        child: Card(
+                            elevation: 10,
+                            child: ListTile(
+                              leading: Icon(Icons.arrow_back_outlined),
+                              tileColor: Colors.greenAccent[100],
+                              title: Text(
+                                'Return to last page',
+                                style: TextStyle(
+                                    fontFamily: "Nunito",
+                                    fontSize: 25,
+                                    color: Colors.purple),
+                              ),
+                            ))),
+                  ],
+                )),
+            body: CustomScrollView(slivers: <Widget>[
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    Image.network(
+                        'https://www.upperrhinevalley.com/sites/default/files/public/styles/grid-8_gallery_image/public/content/3401/museum_tinguely_c_pino_musi.jpg?itok=sbtLsnXH'),
+                    Stack(children: [
+                      SizedBox(
+                          height: 150,
+                          width: 400,
+                          child: Card(
+                              elevation: 5,
+                              color: Color.fromRGBO(205, 205, 205, 1.0))),
+                      Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                          child: Text('Website:',
+                              style: TextStyle(
+                                  fontFamily: 'Nunito',
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black))),
+                      Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 40, horizontal: 5),
+                          child: TextButton(
+                              onPressed: () {
+                                launchUrl(url);
+                              },
+                              child: Text('tinguely.ch',
+                                  style: TextStyle(
+                                      fontFamily: 'Nunito',
+                                      fontSize: 25,
+                                      decoration: TextDecoration.underline,
+                                      color: Colors.blue)))),
+                    ]),
+                    Stack(
+                      children: [
+                        SizedBox(
+                            height: 250,
+                            width: 400,
+                            child: Card(
+                                elevation: 5,
+                                color: Color.fromRGBO(205, 202, 202, 1.0))),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 5),
+                            child: Text('Opening Times:',
+                                style: TextStyle(
+                                    fontFamily: 'Nunito',
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black))),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 40, horizontal: 5),
+                            child: Text('Monday: Closed ',
+                                style: TextStyle(
+                                    fontFamily: 'Nunito',
+                                    fontSize: 23,
+                                    color: Colors.black))),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 70, horizontal: 5),
+                            child: Text('Tuesday-Sunday: 11:00 - 18:00 ',
+                                style: TextStyle(
+                                    fontFamily: 'Nunito',
+                                    fontSize: 23,
+                                    color: Colors.black))),
+                        Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 100, horizontal: 5),
+                            child: Text('Thursday: 11:00 - 21:00',
+                                style: TextStyle(
+                                    fontFamily: 'Nunito',
+                                    fontSize: 23,
+                                    color: Colors.black))),
+                      ],
+                    ),
+                    Stack(children: [
+                      SizedBox(
+                          height: 250,
+                          width: 400,
+                          child: Card(
+                              elevation: 5,
+                              color: Color.fromRGBO(205, 202, 202, 1.0))),
+                      Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+                          child: Text('Admission Fees:',
+                              style: TextStyle(
+                                  fontFamily: 'Nunito',
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black))),
+                      Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 30, horizontal: 5),
+                          child: Text('Adults: 18 CHF ',
+                              style: TextStyle(
+                                  fontFamily: 'Nunito',
+                                  fontSize: 23,
+                                  color: Colors.black))),
+                      Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 60, horizontal: 5),
+                          child: Text('Students & Disabled: 12 CHF  ',
+                              style: TextStyle(
+                                  fontFamily: 'Nunito',
+                                  fontSize: 23,
+                                  color: Colors.black))),
+                      Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 90, horizontal: 5),
+                          child: Text('Groups (12+): 12 CHF ',
+                              style: TextStyle(
+                                  fontFamily: 'Nunito',
+                                  fontSize: 23,
+                                  color: Colors.black))),
+                    ]),
+                    Stack(children: [
+                      SizedBox(
+                          height: 250,
+                          width: 400,
+                          child: Card(
+                              elevation: 5,
+                              color: Color.fromRGBO(205, 202, 202, 1.0))),
+                      Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+                          child: Text('Address & Contact:',
+                              style: TextStyle(
+                                  fontFamily: 'Nunito',
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black))),
+                      Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 30, horizontal: 5),
+                          child: Text('Paul Sacher-Anlage 2',
+                              style: TextStyle(
+                                  fontFamily: 'Nunito',
+                                  fontSize: 23,
+                                  color: Colors.black))),
+                      Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 60, horizontal: 5),
+                          child: Text('CH-4002 Basel ',
+                              style: TextStyle(
+                                  fontFamily: 'Nunito',
+                                  fontSize: 23,
+                                  color: Colors.black))),
+                      Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 90, horizontal: 5),
+                          child: Text('Postfach 3255',
+                              style: TextStyle(
+                                  fontFamily: 'Nunito',
+                                  fontSize: 23,
+                                  color: Colors.black))),
+                      Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 115, horizontal: 0),
+                          child: TextButton(
+                              onPressed: () {
+                                // ignore: deprecated_member_use
+                                launch("tel:// +41 61 681 93 20");
+                              },
+                              child: Text('Museum: +41 61 681 93 20',
+                                  style: TextStyle(
+                                      fontFamily: 'Nunito',
+                                      fontSize: 25,
+                                      decoration: TextDecoration.underline,
+                                      color: Colors.blue)))),
+                      Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 150, horizontal: 0),
+                          child: TextButton(
+                              onPressed: () {
+                                // ignore: deprecated_member_use
+                                launch("tel:// +41 61 688 94 58");
+                              },
+                              child: Text('Bistro: +41 61 688 94 58',
+                                  style: TextStyle(
+                                      fontFamily: 'Nunito',
+                                      fontSize: 25,
+                                      decoration: TextDecoration.underline,
+                                      color: Colors.blue))))
+                    ]),
+                  ],
+                ),
+              ),
+            ])));
   }
 }
