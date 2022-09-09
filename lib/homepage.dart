b3d66cd4f2a47002031a43ac59ec970eccc05a1b
TommmGB <112927088+TommmGB@users.noreply.github.com>
2022-09-09 12:24:24 +0200

    Update homepage.dart

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -127,7 +127,7 @@ class Homepage extends StatelessWidget {
                       borderRadius: BorderRadius.only(
                           bottomRight: Radius.circular(5),
                           topRight: Radius.circular(5)),
-                      side: BorderSide(width: 2, color: Colors.black)),
+                      side: BorderSide(width: 3, color: Colors.black)),
                   child: ListTile(
                     leading: Icon(
                       Icons.location_on_outlined,
@@ -156,12 +156,12 @@ class Homepage extends StatelessWidget {
                               borderRadius: BorderRadius.only(
                                   bottomRight: Radius.circular(5),
                                   topRight: Radius.circular(5)),
-                              side: BorderSide(width: 5, color: Colors.black)),
+                              side: BorderSide(width: 3, color: Colors.black)),
                           child: Image.network(
                               'https://cdn.tomas-travel.com/tds/repository/TDS00020012496817936/TDS00020010010367324/TDS00020013696921880_sized_800_0.jpg',
                               fit: BoxFit.cover)))),
               Container(
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 2),
                 child: const Card(
                     borderOnForeground: true,
                     shape: RoundedRectangleBorder(
@@ -189,12 +189,12 @@ class Homepage extends StatelessWidget {
                         borderRadius: BorderRadius.only(
                             bottomRight: Radius.circular(5),
                             topRight: Radius.circular(5)),
-                        side: BorderSide(width: 5, color: Colors.black)),
+                        side: BorderSide(width: 3, color: Colors.black)),
                     child: Image.network(
                         'https://flatfox.ch/media/medialibrary/2019/05/Munster_858x483px.jpg',
                         fit: BoxFit.cover))),
             Container(
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 2),
                 child: const Card(
                     borderOnForeground: true,
                     shape: RoundedRectangleBorder(
@@ -217,7 +217,7 @@ class Homepage extends StatelessWidget {
                       borderRadius: BorderRadius.only(
                           bottomRight: Radius.circular(5),
                           topRight: Radius.circular(5)),
-                      side: BorderSide(width: 2, color: Colors.black)),
+                      side: BorderSide(width: 3, color: Colors.black)),
                   child: ListTile(
                     leading: Icon(
                       Icons.navigation_outlined,
@@ -243,12 +243,12 @@ class Homepage extends StatelessWidget {
                               borderRadius: BorderRadius.only(
                                   bottomRight: Radius.circular(5),
                                   topRight: Radius.circular(5)),
-                              side: BorderSide(width: 5, color: Colors.black)),
+                              side: BorderSide(width: 3, color: Colors.black)),
                           child: Image.network(
                               'https://www.bvb.ch/de/wp-content/uploads/sites/2/2022/05/Flexity.png',
                               fit: BoxFit.cover)))),
               Container(
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 2),
                 child: const Card(
                     borderOnForeground: true,
                     shape: RoundedRectangleBorder(
@@ -277,12 +277,12 @@ class Homepage extends StatelessWidget {
                               borderRadius: BorderRadius.only(
                                   bottomRight: Radius.circular(5),
                                   topRight: Radius.circular(5)),
-                              side: BorderSide(width: 5, color: Colors.black)),
+                              side: BorderSide(width: 3, color: Colors.black)),
                           child: Image.network(
                               'https://map.viamichelin.com/map/carte?map=viamichelin&z=10&lat=47.55961&lon=7.58061&width=550&height=382&format=png&version=latest&layer=background&debug_pattern=.*',
                               fit: BoxFit.cover)))),
               Container(
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 2),
                 child: const Card(
                   borderOnForeground: true,
                   shape: RoundedRectangleBorder(
